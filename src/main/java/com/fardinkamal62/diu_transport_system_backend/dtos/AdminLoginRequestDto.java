b6d6058ac3906612100a7ea9af058a7b4cd7fd30
Fardin Kamal <fardinkamal62@protonmail.ch>
2025-11-28 21:42:29 +0600

    refactor: update password validation regex and adjust HikariCP connection pool settings

--- src/main/java/com/fardinkamal62/diu_transport_system_backend/dtos/AdminLoginRequestDto.java
+++ src/main/java/com/fardinkamal62/diu_transport_system_backend/dtos/AdminLoginRequestDto.java
@@ -21,7 +21,7 @@ public class AdminLoginRequestDto {
     @Setter
     @NotBlank(message = "Password is required")
     @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&.])[A-Za-z\\d@$!%*?&.]+$",
             message = "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character")
     private String password;
 
